2f63ed3018c30886c091d40c60b94ce022b773e6
SouvikS <souvik.sarkar906@gmail.com>
2024-03-17 12:08:11 +0530

    added project

---
+++ src/Programs/me.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class me {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Your first argument is: "+args[0]);  
+		System.out.println("Your second argument is: "+args[1]);  
+		System.out.println("Your third argument is: "+args[2]);  
+	}
+
+}
---
+++ src/Programs/tEST1.java
@@ -0,0 +1,10 @@
+package Programs;
+
+public class tEST1 {
+
+	public static void main(String[] args) {
+		System.out.println("3"+1);
+
+	}
+
+}
